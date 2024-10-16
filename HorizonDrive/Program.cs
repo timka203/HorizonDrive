702dced44e5532fecaf4ea3c251d96b22666e3f1
Kintimak <tpashakulov@gmail.com>
2024-10-16 19:39:13 +0200

    Adding color and size change

--- HorizonDrive/Program.cs
+++ HorizonDrive/Program.cs
@@ -11,51 +11,32 @@ using NAudio;
 using System.Linq;
 using System.Threading;
 using AForge.Math;
+using System.Threading.Tasks;
 
 namespace HorizonDrive
-{
-	class Program
-	{
-        static void Main(string[] args)
+{ 
+    class AudioVisualizer
+    {
+        public char division = '#';
+        private static string tmp = "";
+        static int inverted_size = 20;
+        public void ConsoleAudioVisualizer(char division = '#')
         {
- 
-        // Printing the current dimensions
-            Console.WriteLine(Console.WindowWidth);
-            Console.WriteLine(Console.WindowHeight);
-            double[] signal = FftSharp.SampleData.SampleAudio1();
-            int sampleRate = 48_000;
-            Console.CursorVisible = false;
-            Console.ForegroundColor = ConsoleColor.Green;
-            // calculate the power spectral density using FFT
-            System.Numerics.Complex[] spectrum = FftSharp.FFT.Forward(signal);
-            double[] psd = FftSharp.FFT.Power(spectrum);
-            double[] freq = FftSharp.FFT.FrequencyScale(psd.Length, sampleRate);
-
-            // plot the sample audio
-            ScottPlot.Plot plt = new ScottPlot.Plot();
-            ScottPlot.Plot myPlot = new();
-            plt.Add.Scatter(freq, psd);
-            plt.YLabel("Power (dB)");
-            plt.XLabel("Frequency (Hz)");
-            plt.SavePng("quickstart.png", 1200, 1800);
-
-            var waveIn = new NAudio.Wave.WaveInEvent
+            Thread thr2 = new Thread(AudioVisualizer.ConsoleInput);
+            thr2.Start();
+            this.division = division;
+            WaveInEvent waveIn = new NAudio.Wave.WaveInEvent
             {
                 DeviceNumber = 1, // indicates which microphone to use
                 WaveFormat = new NAudio.Wave.WaveFormat(rate: 44100, bits: 16, channels: 1),
-                BufferMilliseconds = 20
+                BufferMilliseconds = 22
             };
             waveIn.DataAvailable += WaveIn_DataAvailable;
             waveIn.StartRecording();
 
-            Console.WriteLine("C# Audio Level Meter");
-            Console.WriteLine("(press any key to exit)");
-            Console.ReadLine();
-
-
         }
 
-        static void WaveIn_DataAvailable(object? sender, NAudio.Wave.WaveInEventArgs e)
+        void  WaveIn_DataAvailable(object? sender, NAudio.Wave.WaveInEventArgs e)
         {
             Int16[] values = new Int16[1024];
             Buffer.BlockCopy(e.Buffer, 0, values, 0, e.Buffer.Length);
@@ -66,24 +47,97 @@ namespace HorizonDrive
                 complices.Add(item);
             }
             var tmp = complices.ToArray();
-        
+
             System.Numerics.Complex[] spectrum = FftSharp.FFT.Forward(tmp);
             double[] psd = FftSharp.FFT.Power(spectrum);
             double[] freq = FftSharp.FFT.FrequencyScale(psd.Length, sampleRate);
-            int inverted_size = 10;
-            for (int i = 0; i < psd.Length- inverted_size; i+= inverted_size)
+            for (int i = 0; i < psd.Length - inverted_size; i += inverted_size)
             {
-                string bar2 = "#";
+                string bar = division.ToString();
                 var count = psd.ToList().GetRange(i, inverted_size).Average();
-                if ( count > 0)
+                if (count > 0)
                 {
-                    bar2 = new('#', (int)(count));
+                    bar = new(division, (int)(count));
                 }
-                Console.WriteLine(bar2);
-
+                Console.WriteLine(bar);
             }
+            Console.WriteLine(AudioVisualizer.tmp);
             Thread.Sleep(1);
             Console.Clear();
         }
+        static void ConsoleInput()
+        {
+            while(true)
+            {
+                char keyPressed = Console.ReadKey().KeyChar;
+                AudioVisualizer.tmp += keyPressed;
+                CheckCommand();
+            }
+            
         }
+
+        static void CheckCommand()
+        {
+            if (tmp.Contains("color"))
+            {
+                if (tmp.Contains("red"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    tmp = "";
+                }
+                if (tmp.Contains("green"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    tmp = "";
+                }
+                if (tmp.Contains("cyan"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    tmp = "";
+                }
+                if (tmp.Contains("magenta"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    tmp = "";
+                }
+
+            }
+            if (tmp.Contains("size"))
+            {
+                try
+                {
+                    var x = tmp.Remove(tmp.Length - 4, 4);
+                    double test;
+                    inverted_size = Double.TryParse(x,out test) ? Convert.ToInt32(x) : 20;
+                }
+                catch (Exception)
+                {
+                }
+                tmp = "";
+                
+
+            }
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.CursorVisible = false;
+            // Printing the current dimensions
+            Console.ForegroundColor = ConsoleColor.Green;
+            AudioVisualizer audioVisualizer = new AudioVisualizer();
+            audioVisualizer.ConsoleAudioVisualizer();
+
+            Console.WriteLine("C# Audio Level Meter");
+            Console.WriteLine("(press any key to exit)");
+            Console.ReadLine();
+
+
+        }
+    }
 }
